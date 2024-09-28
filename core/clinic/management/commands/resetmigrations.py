2def8d8d44927a2466c8b81c22b33c0c13ccb90d
Jesus M <jmolinap2@unemi.edu.ec>
2024-09-28 14:27:04 -0500

    ward

--- core/clinic/management/commands/resetmigrations.py
+++ core/clinic/management/commands/resetmigrations.py
@@ -1,17 +1,22 @@
 # Guarda esto como un archivo dentro de cualquier aplicación Django en la carpeta management/commands
 
 
-from django.core.management.base import BaseCommand
+ffrom django.core.management.base import BaseCommand
 import os
 
 class Command(BaseCommand):
     help = 'Deletes all migration files in all apps'
 
     def handle(self, *args, **options):
-        project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        # Obtiene la ruta absoluta del archivo actual (__file__ es el archivo actualmente ejecutado).
+        current_file = os.path.abspath(__file__)
+        project_root = os.path.dirname(os.path.dirname(os.path.dirname(current_file)))
+
         for subdir, dirs, files in os.walk(project_root):
             for file in files:
-                filepath = subdir + os.sep + file
-                if filepath.endswith(".py") and 'migrations' in filepath and not file == '__init__.py':
+                filepath = os.path.join(subdir, file)
+                # Añade condición para evitar borrar el archivo actual del comando
+                if filepath.endswith(".py") and 'migrations' in filepath and filepath != current_file and file != '__init__.py':
                     os.remove(filepath)
                     self.stdout.write(self.style.SUCCESS(f'Removed {filepath}'))
+
