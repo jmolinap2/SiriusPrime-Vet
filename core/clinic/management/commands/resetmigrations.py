e5d4687cfe5c83d4dc12a017391743b9d74459ca
Jesus M <jmolinap2@unemi.edu.ec>
2024-09-28 21:14:42 -0500

    automatic

--- core/clinic/management/commands/resetmigrations.py
+++ core/clinic/management/commands/resetmigrations.py
@@ -1,25 +1,44 @@
-# Guarda esto como un archivo dentro de cualquier aplicación Django en la carpeta management/commands
+
+
 
 from django.core.management.base import BaseCommand
+from django.conf import settings  # Importa las configuraciones de Django
 import os
 
 class Command(BaseCommand):
-    help = 'Deletes all migration files in all apps'
+    help = 'Elimina todos los archivos de migración en todas las apps'
 
     def handle(self, *args, **options):
-        project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        
+        # Usamos settings.BASE_DIR para obtener la raíz del proyecto
+        project_root = settings.BASE_DIR
+        virtual_env = os.environ.get('VIRTUAL_ENV')  # Obtén la ruta del entorno virtual si existe
+        self.stdout.write(self.style.NOTICE(f"Raíz del proyecto: {project_root}"))
+        if virtual_env:
+            self.stdout.write(self.style.NOTICE(f"Entorno virtual detectado: {virtual_env}"))
+
+        # Recorrer todos los subdirectorios y archivos
         for subdir, dirs, files in os.walk(project_root):
+            # Ignorar el directorio del entorno virtual si está definido
+            if virtual_env and subdir.startswith(virtual_env):
+                continue
+
+            self.stdout.write(self.style.NOTICE(f"Revisando directorio: {subdir}"))
             for file in files:
-                filepath = subdir + os.sep + file
-                if filepath.endswith(".py") and 'migrations' in filepath and not file == '__init__.py':
-                    # Muestra el archivo que se va a eliminar y pide confirmación
+                # Crear la ruta completa del archivo
+                filepath = os.path.join(subdir, file)
+                
+                # Verificar si el archivo es una migración y no es __init__.py
+                if filepath.endswith(".py") and 'migrations' in filepath and file != '__init__.py':
+                    # Confirmar antes de eliminar
                     confirm = input(f"¿Quieres eliminar este archivo de migración? {filepath} (s/n): ").lower()
                     
                     if confirm == 's':
                         os.remove(filepath)
-                        self.stdout.write(self.style.SUCCESS(f"Removed {filepath}"))
+                        self.stdout.write(self.style.SUCCESS(f"Eliminado {filepath}"))
                     else:
                         self.stdout.write(self.style.WARNING(f"No se eliminó {filepath}"))
 
+        # Mensaje final cuando termine el proceso
         self.stdout.write(self.style.SUCCESS("Proceso completado."))
+
+
